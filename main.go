385d4d5f45a82cc56d7babcae7ee7a09a45c4397
meunomeebero <mail@bero.land>
2025-10-02 17:31:46 -0300

    feat: atualizar configuração CORS para expor cabeçalhos e definir MaxAge

--- main.go
+++ main.go
@@ -53,9 +53,12 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowAllOrigins: true,
-		AllowMethods:    []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		AllowHeaders:    []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
 	}))
 
 	router.GET("/", func(c *gin.Context) {
